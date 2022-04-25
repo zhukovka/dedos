127f47ccedb6989b7d48c1dcb0b0b2b11374fce8
Lena Zhukova <olenazh@wix.com>
2022-04-25 17:39:08 +0200

    added webpack-dev-middleware https://webpack.js.org/guides/development/

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,22 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js'
+        index: './src/index.js',
+        hot: 'webpack/hot/dev-server.js',
+        client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
     },
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
+        hot: false,
+        client: false
     },
     plugins: [
-        new HtmlWebpackPlugin({title: "Dedo"})
+        new HtmlWebpackPlugin({title: "Dedo"}),
+        new webpack.HotModuleReplacementPlugin(),
     ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+        publicPath: '/',
     },
     optimization: {
         runtimeChunk: 'single',
