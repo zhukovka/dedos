5684dfba415206a3f0a04ec374f66bf011ada05b
Lena Zhukova <olenazh@wix.com>
2022-04-25 17:12:30 +0200

    added assets management https://webpack.js.org/guides/asset-management/

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { createRoot } from "react-dom/client";
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// const root = createRoot(rootElement);
+import './style.css';
+import Dedo from './dedo.webp';
+
+console.log(Dedo);
