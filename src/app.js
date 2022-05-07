037947dd8f937b8d861a67efc72528e08ea30390
Lena Zhukova <olenazh@wix.com>
2022-05-07 20:42:09 +0200

    app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
-//kuku
+function loadScript(href) {
+    if(href === '/'){
+        return;
+    }
+    import(`${href}.js`)
+        .then(({default: dedo}) => {
+            // Do something with the module.
+            document.getElementById('suspense').innerHTML = dedo();
+        });
+}
+
+function goTo() {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    console.log(href);
+    history.pushState({}, document.title, href);
+    loadScript(href);
+}
+
+document.getElementById('root').innerHTML = `
+    <div class="App">
+            <h1 class='dedo'>DEDOS 🖐</h1>
+            <ol>
+            <li><a href="/polegar" onclick="goTo()">Polegar</a></li>
+            <li><a href="/indicador" onclick="goTo()">Indicador</a></li>
+            <li><a href="/medio" onclick="goTo()">Dedo médio</a></li>
+            <li><a href="/anelar" onclick="goTo()">Anelar</a></li>
+            <li><a href="/minimo" onclick="goTo()">Dedo mínimo</a></li>
+            </ol>
+            <div id='suspense'></div>
+        </div>
+`
