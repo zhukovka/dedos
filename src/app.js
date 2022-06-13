fa066bd6038ba9b541d23a15e99bce75668f0a62
Lena Zhukova <olenazh@wix.com>
2022-06-13 16:19:45 +0200

    format

--- src/app.js
+++ src/app.js
@@ -5,18 +5,18 @@ export const App = (props) => {
     }
     return `
         <div class="App">
-                <h1 class='dedo'>DEDOS 🖐</h1>
-                <ol>
-                    <li><a href="/polegar" onclick="goTo()">Polegar</a></li>
-                    <li><a href="/indicador" onclick="goTo()">Indicador</a></li>
-                    <li><a href="/medio" onclick="goTo()">Dedo médio</a></li>
-                    <li><a href="/anelar" onclick="goTo()">Anelar</a></li>
-                </ol>
-                <div id='pages'>
-                    <!--  Sync in SSR -->
-                    ${content}
-                </div>
+            <h1 class='dedo'>DEDOS 🖐</h1>
+            <ol>
+                <li><a href="/polegar" onclick="goTo()">Polegar</a></li>
+                <li><a href="/indicador" onclick="goTo()">Indicador</a></li>
+                <li><a href="/medio" onclick="goTo()">Dedo médio</a></li>
+                <li><a href="/anelar" onclick="goTo()">Anelar</a></li>
+            </ol>
+            <div id='pages'>
+                <!--  Sync in SSR -->
+                ${content}
             </div>
+        </div>
         `;
 };
 
